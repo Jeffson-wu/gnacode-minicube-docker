7f8df4012dd59145595ac556d50b1a5ecf0a04ab
Leonid Gonchar <goncharleonid@gmail.com>
2020-08-23 20:21:29 +0300

    MC-36: Introduce BUILD_VARIANT variable
    Signed-off-by: developer <developer@biovicesystems.com>

--- scripts/jenkins-build.sh
+++ scripts/jenkins-build.sh
@@ -4,7 +4,10 @@
 if [ -z "${BRANCH}" ]; then 
     BRANCH=dev
 fi
-
+if [ -z "$BUILD_VARIANT" ]; then 
+    BUILD_VARIANT=develop
+fi
+SET_BUILD_VARIANT="BUILD_VARIANT = \\\"${BUILD_VARIANT}\\\""
 OUTPUT_DIR=$(pwd)/output
 
 mkdir -p ${OUTPUT_DIR}
@@ -14,6 +17,9 @@ docker run -i --rm -v jenkins-data:/var/jenkins_home biovices/yocto:latest /bin/
     export MACHINE=imx28minicube && \
     repo init -u git@bitbucket.org:gnateam/gnacode-minicube-bsp-platform.git -b ${BRANCH} && \
     repo sync && \
+    source ./setup-environment build && \
+    cd ./.. && \
+    echo ${SET_BUILD_VARIANT} >> ./build/conf/local.conf  && \
     source ./setup-environment build && \
     bitbake core-image-minicube && \
     rm -rf ${OUTPUT_DIR}/* && \
